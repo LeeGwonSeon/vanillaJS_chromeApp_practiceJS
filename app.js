6ba42d8309c6cf0b9ccca50270780ac628ec9f4f
GwonseonLee <verygoody2k@hotmail.com>
2023-04-13 18:49:47 +0900

    CSS in Javascript part Three

--- app.js
+++ app.js
@@ -5,48 +5,12 @@ console.dir(h1);
 h1.innerText = "Got it!";
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor
-    if (currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor = "blue";
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor
 }
 
-// function handleMouseEnter() {
-//     h1.innerText = "Mouse is here!";
-// }
-
-// function handleMouseLeave() {
-//     h1.innerText = "Mouse is gone!";
-// }
-
-// function handleWindowResize() {
-//     document.body.style.backgroundColor = "tomato";
-// }
-
-// function handleWindowCopy() {
-//     alert("copier!");
-// }
-
-// function handleWindowOffline() {
-//     alert("SOS no WIFI");
-// }
-// function handleWindowOnline() {
-//     alert("All good!!!!");
-// }
-
-// title.onclick = handleTitleClick;
-// title.onmouseenter = handleMouseEnter;
-// title.onmouseleave = handleMouseLeave;
-
 h1.addEventListener("click", handleTitleClick);
-// h1.addEventListener("mouseenter", handleMouseEnter);
-// h1.addEventListener("mouseleave", handleMouseLeave);
-
-// window.addEventListener("resize", handleWindowResize);
-// window.addEventListener("copy", handleWindowCopy);
-// window.addEventListener("offline", handleWindowOffline);
-// window.addEventListener("online", handleWindowOnline);
