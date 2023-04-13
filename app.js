cd42bfaab5f9b4f77c76635fa748b537c6ce6470
GwonseonLee <verygoody2k@hotmail.com>
2023-04-13 16:23:47 +0900

    JavaScript_CSS in javascript

--- app.js
+++ app.js
@@ -5,41 +5,48 @@ console.dir(h1);
 h1.innerText = "Got it!";
 
 function handleTitleClick() {
-    h1.style.color = "red";
+    const currentColor = h1.style.color;
+    let newColor
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
+// function handleMouseEnter() {
+//     h1.innerText = "Mouse is here!";
+// }
 
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
+// function handleMouseLeave() {
+//     h1.innerText = "Mouse is gone!";
+// }
 
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
+// function handleWindowResize() {
+//     document.body.style.backgroundColor = "tomato";
+// }
 
-function handleWindowCopy() {
-    alert("copier!");
-}
+// function handleWindowCopy() {
+//     alert("copier!");
+// }
 
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-function handleWindowOnline() {
-    alert("All good!!!!");
-}
+// function handleWindowOffline() {
+//     alert("SOS no WIFI");
+// }
+// function handleWindowOnline() {
+//     alert("All good!!!!");
+// }
 
 // title.onclick = handleTitleClick;
 // title.onmouseenter = handleMouseEnter;
 // title.onmouseleave = handleMouseLeave;
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
+// h1.addEventListener("mouseenter", handleMouseEnter);
+// h1.addEventListener("mouseleave", handleMouseLeave);
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+// window.addEventListener("resize", handleWindowResize);
+// window.addEventListener("copy", handleWindowCopy);
+// window.addEventListener("offline", handleWindowOffline);
+// window.addEventListener("online", handleWindowOnline);
